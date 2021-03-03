ca021a29deb3c79460e5c4341be83a3e61317c1c
duckchanahn <tkdgur5031@naver.com>
2021-03-03 17:14:14 +0900

    create_module_dataPre

---
+++ Diary_Back/src/main/java/constants/eAddress.java
@@ -0,0 +1,20 @@
+package constants;
+
+public class eAddress {
+
+    public enum eDistrict_k {
+        강남구, 강동구, 강북구, 강서구, 관악구,
+        광진구, 구로구, 금천구, 노원구, 도봉구,
+        동대문구, 동작구, 마포구, 서대문구, 서초구,
+        성동구, 성북구, 송파구, 양천구, 영등포구,
+        용산구, 은평구, 종로구, 중구, 중랑구
+    };
+
+    public enum eDistrict_eng {
+        Gangnam, Gangdong, Gangbuk, Gangseo, Gwanak,
+        Gwangjin, Guro, Geumcheon, Nowon, Dobong,
+        Dongdaemun, Dongjak, Mapo, Seodaemun, Seocho,
+        Seongdong, Seongbuk, Songpa, Yangcheon, Yeongdeungpo,
+        Yongsan, Eunpyeong, Jongno, Jung, Jungnang
+    };
+}
