ed521b5a49ab42027c8ca5def0a28437a8a98319
duckchanahn <tkdgur5031@naver.com>
2021-04-02 20:35:13 +0900

    test repositories split

--- Diary_Back/src/main/java/controller/homeController.java
+++ Diary_Back/src/main/java/controller/homeController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class homeController {
 
+
+
+
+
     @Autowired
     private service.tempService tempService;
 
     @RequestMapping(value = "/")
     public String home() {
+        String test = "이게된거야?";
         return "index";
     }
 
